f14da7db79d957a30fb7fd9bf96b6ab682ab2b84
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-11 16:15:19 +0900

    inference script

--- inference.py
+++ inference.py
@@ -1,5 +1,4 @@
 # Import libraries
-
 from PIL import Image
 import torch, cv2
 from torchvision import transforms as T
@@ -16,14 +15,19 @@ model = torch.load("test.pth")
 # Switch the model into evaluation mode
 model.eval()
 
+# Read an image
 im = Image.open("bear.jpg")
-tfs = T.Compose([T.Resize((384, 384)), T.ToTensor()])
-inp = tfs(im)
-out = torch.nn.functional.softmax(model(inp.unsqueeze(0)), dim = -1)
 
+# Initialize transformations
+tfs = T.Compose([T.Resize((384, 384)), T.ToTensor()])
+
+# Apply transformations and input to the model
+out = torch.nn.functional.softmax(model(tfs(im).unsqueeze(0)), dim = -1)
+
+# Get topk values and indices
 vals, ids = out[0].topk(k)
 
+# Go through topk values and indices
 for idx, (val, id) in enumerate(zip(vals, ids)):
-    
     lbl = classes[id.item()].strip()
     print(f"Top {idx+1} -> {lbl} with a score of {val.item():.3f}")
