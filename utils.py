1505acf4a3bb9598fdc9485e8930019fde3b2e15
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-05-04 11:28:07 +0900

    helper functions

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+# Import library
 import numpy as np
 
+# A function to get number of trainable parameters of a model
 def get_n_params(model): return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+# A function to assert that two tensors are equal
 def assert_tensors_equal(t1, t2): np.testing.assert_allclose(t1.detach().numpy(), t2.detach().numpy())
